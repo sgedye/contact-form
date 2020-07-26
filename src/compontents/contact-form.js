b0a997e36007243bbfdbefe955b7b89311883450
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-26 17:26:03 +0800

    :construction: Added form checking

--- src/compontents/contact-form.js
+++ src/compontents/contact-form.js
@@ -8,32 +8,25 @@ const ContactForm = () => {
   // const [name, setName] = useState('');
   // const [email, setEmail] = useState('');
   // const [message, setMessage] = useState('');
-  let name, email, message;
-
   const [errors, setErrors] = useState({
     name: '', email: '', message: ''
   });
 
-  // useEffect(() => {
-    //   setName(() => nameInput.innerText);
-    //   console.log(nameInput.innerText)
-    // }, [nameInput.innerText])
-
   const handleChange = e => {
     e.preventDefault();
     const { name, value } = e.target;
-    let errorsCopy = {...errors}
+    const errorsCopy = {...errors};
     switch (name) {
       case "name":
         const nameRegex = RegExp(/^[a-zA-Z\s-]*$/)
         if (value.length < 3) {
-          errorsCopy[name] = `Names must be at least 3 characters long.`;
+          errorsCopy[name] = `Your name must be at least 3 characters long.`;
         } else if (value.length > 25) {
           errorsCopy[name] = `Maximum name is 25 characters, you've entered ${value.length} characters.`;
         } else if (!nameRegex.test(value)) {
           errorsCopy[name] = `Only letters, whitespace and dashes (-) are accepted.`;
         } else {
-          errorsCopy[name] = '';
+          errorsCopy[name] = 'OK';
         }
         break;
       case "email":
@@ -44,49 +37,65 @@ const ContactForm = () => {
         if (value.length < 6) {
           errorsCopy[name] = `Emails must be atleast 6 characters long.`;
         } else if (value.length > 25) {
-          errorsCopy[name] = `Maximum name is 25 characters, you've entered ${value.length} characters.`;
+          errorsCopy[name] = `${value.length} characters is more than the maximum length of 25.`;
         } else if(!illegalChars.test(value)) {
           errorsCopy[name] = `Invalid character entered.`;
         } else if (!acceptEmail.test(value)) {
-          errorsCopy[name] = `Only valid email addresses are accepted`;
+          errorsCopy[name] = `Only valid email addresses are accepted.`;
         } else {
-          errorsCopy[name] = '';
+          errorsCopy[name] = 'OK';
         }
         break;
       case "message":
         const messageRegex = RegExp(/^[a-zA-Z0-9.,?&!/'"-\s]+$/);
         if (value.length < 10) {
-          errorsCopy[name] = `Message must be atleast 10 characters long.`
+          errorsCopy[name] = `Message must be atleast 10 characters long.`;
         } else if (value.length > 250) {
-          errorsCopy[name] = `${value.length} characters is more than the maximum length of 250.`
+          errorsCopy[name] = `${value.length} characters is more than the maximum length of 250.`;
         } else if (!messageRegex.test(value)) {
-          errorsCopy[name] = `An invalid character has been entered.`
+          errorsCopy[name] = `An invalid character has been entered.`;
         } else {
-          errorsCopy[name] = '';
+          errorsCopy[name] = 'OK';
         }
         break;
       default:
         break;
     }
-    setErrors({...errors, [name]: errorsCopy[name]})
+    setErrors({...errors, [name]: errorsCopy[name]});
+    const submitBtn = document.getElementById('submit-btn');
+    submitBtn.style.backgroundColor = 'blue';
+
   }
 
-  const validForm = () => {
-    let valid = true;
+  const validateForm = () => {
+    let isValid = true;
     Object.values(errors).forEach(value => {
-      value.length > 0 && (valid = false);
+      value !== 'OK' && (isValid = false);
     });
-    return valid;
+    return isValid;
   }
 
   const handleSubmit = e => {
     e.preventDefault();
-
-    if (validForm()) {
-      console.log(name, email, message);
+    const isValid = validateForm();
+    if (isValid) {
+      console.log('Happy Dayzzz')
+      // const mailSent = new Audio("./mailSentSound.mp3");
+      // mailSent.play();
+      new Audio("./mailSentSound.mp3").play()
     } else {
-      console.log('Form Error');
+      const errorsCopy = {...errors};
+      for (const [key, value] of Object.entries(errors)) {
+        if (!value) {
+          errorsCopy[key] = 'Please fill in this input.';
+        }
+      }
+      setErrors({ ...errors, ...errorsCopy });
     }
+    const submitBtn = document.getElementById('submit-btn');
+    submitBtn.style.backgroundColor = (isValid) ? 'blue' : 'grey';
+    console.log(isValid)
+
   }
 
   return (
@@ -118,9 +127,11 @@ const ContactForm = () => {
                 </label>
                 <input
                   id="form-name"
-                  className={classnames(styles.formInput, {
-                    [styles.formError]: errors.name,
-                  })}
+                  className={classnames(styles.formInput,
+                    errors.name === 'OK' 
+                      ? styles.inputSuccess 
+                      : errors.name ? styles.inputError : ''
+                  )}
                   type="text"
                   name="name"
                   placeholder="Enter your name..."
@@ -128,9 +139,9 @@ const ContactForm = () => {
                 />
                 <div className={styles.errorWrapper}>
                   <code
-                    className={classnames(styles.errorMessage, {
-                      [styles.visible]: errors.name,
-                    })}
+                    className={classnames(styles.errorMessage,
+                      (errors.name && errors.name !== 'OK') ? styles.visible : ''
+                    )}
                   >
                     {errors.name}
                   </code>
@@ -142,9 +153,11 @@ const ContactForm = () => {
                 </label>
                 <input
                   id="form-email"
-                  className={classnames(styles.formInput, {
-                    [styles.formError]: errors.email,
-                  })}
+                  className={classnames(styles.formInput,
+                    errors.email === 'OK'
+                      ? styles.inputSuccess
+                      : errors.email ? styles.inputError : ''
+                  )}
                   type="email"
                   name="email"
                   placeholder="Enter your email..."
@@ -152,9 +165,9 @@ const ContactForm = () => {
                 />
                 <div className={styles.errorWrapper}>
                   <code
-                    className={classnames(styles.errorMessage, {
-                      [styles.visible]: errors.email,
-                    })}
+                    className={classnames(styles.errorMessage,
+                      (errors.email && errors.email !== 'OK') ? styles.visible : ''
+                    )}
                   >
                     {errors.email}
                   </code>
@@ -165,9 +178,11 @@ const ContactForm = () => {
                   Message:
                 </label>
                 <textarea
-                  className={classnames(styles.formTextArea, {
-                    [styles.formError]: errors.message,
-                  })}
+                  className={classnames(styles.formTextArea,
+                    errors.message === 'OK'
+                      ? styles.inputSuccess
+                      : errors.message ? styles.inputError : ''
+                  )}
                   cols="16"
                   rows="3"
                   name="message"
@@ -176,9 +191,9 @@ const ContactForm = () => {
                 ></textarea>
                 <div className={styles.errorWrapper}>
                   <code
-                    className={classnames(styles.errorMessage, {
-                      [styles.visible]: errors.message,
-                    })}
+                    className={classnames(styles.errorMessage,
+                      (errors.message && errors.message !== 'OK') ? styles.visible : ''
+                    )}
                   >
                     {errors.message}
                   </code>
@@ -186,6 +201,7 @@ const ContactForm = () => {
               </li>
               <li className={styles.listItem}>
                 <input
+                  id="submit-btn"
                   className={styles.submitButton}
                   type="submit"
                   value="Submit"
